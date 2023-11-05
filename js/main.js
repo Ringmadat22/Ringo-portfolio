ea5827b505466a30aeb26a767b613df91b7b99c7
Ring Madat <smithemma329@gmail.com>
2023-11-05 09:36:49 +0300

    main

--- js/main.js
+++ js/main.js
@@ -108,3 +108,43 @@
     
 })(jQuery);
 
+$(function() {
+    $(".p2").typed({
+      strings: ["DESIGNER", "DEVELOPER", "CREATIVE"],
+      typeSpeed: 10,
+      backSpeed: 2,
+      backDelay: 200,
+      showCursor: false,
+      loop: false
+    });
+  });
+  
+  $('.hero-down').mousedown(function() {
+    TweenMax.fromTo('.btn-react', 0.25, {
+      opacity: 0,
+      scale: 0
+    }, {
+      opacity: 0.25,
+      scale: 1,
+      onComplete: function() {
+        TweenMax.to('.btn-react', 0.25, {
+          opacity: 0,
+          scale: 0
+        });
+      }
+    });
+  });
+  
+  // smooth scroll to div
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
